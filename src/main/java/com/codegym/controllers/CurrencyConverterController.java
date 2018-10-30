79dbc98da1c9c1c0284c61f8763e632e7eb161ac
SON NGUYEN <myantj3m@gmail.com>
2018-10-30 21:41:46 +0700

    first commit

---
+++ src/main/java/com/codegym/controllers/CurrencyConverterController.java
@@ -0,0 +1,23 @@
+package com.codegym.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CurrencyConverterController {
+    @GetMapping("/converter")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/usd")
+    public String submit(@RequestParam String usd,String rate, Model model) {
+        float result = Float.parseFloat(usd)*Float.parseFloat(rate);
+        model.addAttribute("result1", result);
+        return "submit";
+    }
+}
